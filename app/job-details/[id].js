9bbe31f1417c17174fbdebe2ad51461666c15adc
xapis-21 <xapisofficial@gmail.com>
2024-01-03 14:14:46 +0300

    my app

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -7,7 +7,7 @@ import {
 } from "react-native";
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
-import { COLORS, icons } from "../../constants";
+import { COLORS, SIZES, icons } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
 import {
@@ -18,6 +18,9 @@ import {
   ScreenHeaderBtn,
   Specifics,
 } from "../../components";
+import { ActivityIndicator } from "react-native";
+
+const tabs = ["About", "Qualifications", "responsibilites"];
 
 const JobDetails = () => {
   const params = useSearchParams();
@@ -27,7 +30,67 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
-  return <Text>Job Details</Text>;
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const onRefresh = () => {};
+
+  return (
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension="60%"
+              handlePress={() => router.back}
+            />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
 };
 
 export default JobDetails;
